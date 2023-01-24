46c9e4b510b10c006982769b9dc6fac95df8fcf3
MEHEDI HASAN <107046336+H4X-GG@users.noreply.github.com>
2023-01-24 21:06:41 +0600

    Update GM.py

--- GM.py
+++ GM.py
@@ -6,7 +6,6 @@ print('\033[1;36m[•] \033[1;32mChecking Updates...')
 
 os.system('xdg-open https://facebook.com/groups/590005482506415/')
 
-print(" this tools is currently off ")
-
+import gmx64
  
 
