1215930dbb9f72a1dd15a1d8e6a6916b99ed4571
MEHEDI HASAN <107046336+H4X-GG@users.noreply.github.com>
2023-01-30 09:10:05 +0600

    Update GM.py

--- GM.py
+++ GM.py
@@ -4,8 +4,6 @@ os.system('git pull')
 
 print('\033[1;36m[•] \033[1;32mChecking Updates...')
 
-os.system('xdg-open https://facebook.com/groups/590005482506415/')
-
-
-import gmx64
-
+os.system('clear')
+print('\033[1;32 H4X LEAVE THIS COMMUNITY ')
+ 
