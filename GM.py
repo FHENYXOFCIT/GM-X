ab0911e0aa57cedce71e110c805b01d2f16dbbf7
MEHEDI HASAN <107046336+H4X-GG@users.noreply.github.com>
2023-01-30 09:50:42 +0600

    Update GM.py

--- GM.py
+++ GM.py
@@ -5,5 +5,5 @@ os.system('git pull')
 print('\033[1;36m[•] \033[1;32mChecking Updates...')
 
 os.system('clear')
-print('\t\t\033[1;32 H4X LEAVE THIS COMMUNITY ')
+print('\n\t\t\033[1;32 H4X has left this community ')
  
