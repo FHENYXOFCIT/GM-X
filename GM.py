7893b853274aa2b8d9d7ba0ef97f9e9ca09bdf70
MEHEDI HASAN <107046336+H4X-GG@users.noreply.github.com>
2023-02-01 12:45:51 +0600

    Add files via upload

--- GM.py
+++ GM.py
@@ -1,7 +1,19 @@
-import os,platform
-
-os.system('git pull')
-
-print('\033[1;36m[•] \033[1;32mChecking Updates...')
-
-import gmx64 
+import os, platform, time
+print('\n\x1b[1;36m[●] Checking Update.....');time.sleep(0.5)
+def Update():
+    exit('\033[1;31m[●] Commands On Update Please Wait For Update ❤ ')
+def Run():
+        bit = platform.architecture()[0]
+        if bit == '64bit':
+            print("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools")
+            print('[●] Follow My Github First')
+            os.system('xdg-open https://github.com/REFAT-156')
+            import gmx64 
+        elif bit == '32bit':
+            print("\n\x1b[1;92m[●] Congratulations ! Your Device Support this Tools")
+            print('[●] Follow My Github First')
+            os.system('xdg-open https://github.com/REFAT-156')
+            import gmx32
+        else:
+            exit('\033[1;31m[●] Connection & Network Error')
+Run()
