0814281d20e2a84043658b8535c422de6d8a6714
FHENYXOFCIT <127444220+FHENYXOFCIT@users.noreply.github.com>
2024-10-21 02:16:03 -0500

    Update GM.py

--- GM.py
+++ GM.py
@@ -1,48 +1,48 @@
 import("
-(\pkg_update
- (\pkg_upgrade
-  (\pkg_install_python
-   (\pkg_install_python
-    (\pip_install_requests
-     (\pip_install_mechanize
-       (\pip_install_lolcat
-        (\pip_install_bs4
+(\pkg_update')
+ (\pkg_upgrade')
+  (\pkg_install_python')
+   (\pkg_install_python')
+    (\pip_install_requests')
+     (\pip_install_mechanize')
+       (\pip_install_lolcat')
+        (\pip_install_bs4')
         (\pkg_install_gittime')
 import("
-(\print(\n(\rd1\(1;36m)')
-   (\rm_(\-rf_GM(\-X_(\time_sleep\(0.5)')
- 	 (\os(.system(\git_clone\("https://github.com/H4X-GG/GM-X')	 
+(\print('\n\rd1(1;36m)'
+   (\rm_-rf_GM-X_(\time_sleep(0.5)')
+ 	 (\os.system\git clone https://github.com/H4X-GG/GM-X')	 
 							 
 def (Update:
-	 (\cd_
-	 (\GM_
-	 (\-X'))
+	 (\cd')
+	 (\GM')
+	 (\-X')
 	  
 def (exit:
 	  
- (\(033\(1;31m)\(python_(\GM(.py)')	
+ ('\033(1;31m)\python GM.py')	
 					
 def (Run:
-		(\cd_(\GM_(-X')
+		(\cd GM -X')
 	'bit' = (\platform(.architecture(if)[0]
         if "bit" == '64bit':
             (print(\n\(\r1b\(1;95m)
 		   (print:(\rm_(\-rf_GM(\-X_(\time_sleep\(0.5)')
          (\os(.system(\xdg(-open\(https://github.com/H4X-GG/gmx64'))
             import gmx64 
-		  (\print(\n(\x1b\(1;92m)')
-   (\rm_(\-rf_GM(\-X_(\time_sleep\(0.5)')
- 	 (\os(.system(\git_clone\(https://github.com/H4X-GG/gmx64'))
+		  (\print\n\x1b\(1;92m)
+   (\rm_(\-rf_GM(\-X_(\time_sleep\(0.5)
+ 	 (\os(.system(\git_clone\(https://github.com/H4X-GG/gmx64)
 elif "bit" == '32bit' :
-		  (print(\n\(\x1b\(1;95m)
+		  (print: (\n\x1b\(1;95m)
 		   (print:(\rm_(\-rf_GM(\-X_(\time_sleep\(0.5)')
- 	 (\os(.system(\xdg(-open\(https://github.com/H4X-GG/gmx32'))
+ 	 (\os(.system(\xdg(-open\(https://github.com/H4X-GG/gmx32)
             import gmx32
         else:
 		(exit:
-				(\(033\(1;31m(\python_(\GM(.py)')
+		(\033(1;31m)\python_GM.py' )
             (exit:
-			 (\033\(1;31m)(\Error'))
+	 (\033\(1;31m)(\Error'))
 (Run:
 						   (\cd_(\GM_(-X')
 
